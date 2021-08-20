a0590c12389bc717fd5dcac7e735bd1900239062
get-Ao <3297984549@qq.com>
2021-08-20 16:46:25 +0800

    静态文件

--- src/cn/student/dao/teacher_Dao.java
+++
@@ -1,7 +0,0 @@
-package cn.student.dao;
-
-import cn.student.entity.teacher_user;
-
-public interface teacher_Dao {
-	public boolean login(teacher_user teacher_user) throws Exception; 
-}
--- src/cn/student/service/teacher_Service.java
+++
@@ -1,7 +0,0 @@
-package cn.student.service;
-
-import cn.student.entity.teacher_user;
-
-public interface teacher_Service {
-	public boolean login(teacher_user teacher_user) throws Exception;
-}
